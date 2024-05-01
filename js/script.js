bbe9511cd1fdcc3dfa2477971252a8717cba1472
helloabdullah <abdullahanjum1999@gmail.com>
2024-05-02 00:54:41 +0500

    abd

---
+++ js/script.js
@@ -0,0 +1,24 @@
+document.getElementById("btn").onclick = function (){
+    document.getElementById("heading").innerHTML = "<h5>Heading 2</h5>"
+
+
+}
+
+// document.getElementById("btn1").onclick = function (){
+//     document.getElementById("heading").innerHTML = "<h2>Heading 2</h2>"
+
+
+// }
+console.log('this is log')
+
+
+var firstName = "Abdullah"
+var lastName = " anjum lala"
+var fullName = firstName + " "+ lastName
+
+
+function printMyName () {
+    console.log (fullName)
+
+} 
+printMyName()
